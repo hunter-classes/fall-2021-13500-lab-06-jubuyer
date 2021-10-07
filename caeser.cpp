02b8baed2e472532bbdbbb04283f30e036bc7a9d
jubuyer <jahmed10@stuy.edu>
2021-10-06 23:43:10 -0400

    Framework for Lab 5, Task B

---
+++ caeser.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include "caeser.h"
+
+// add functions here
+char shiftChar(char c, int rshift) {
+
+}
+
+std::string encryptCaesar(std::string plaintext, int rshift) {
+
+}
