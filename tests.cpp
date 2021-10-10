3dd9fe66df7f0d28b64662b2a304e1d325e01cbb
jubuyer <jahmed10@stuy.edu>
2021-10-09 20:05:16 -0400

    Added basic tests for Caeser cipher

--- tests.cpp
+++ tests.cpp
@@ -3,3 +3,10 @@
 #include "caeser.h"
 
 // add your tests here
+TEST_CASE("Caeser Cipher Theory Tests") {
+  SUBCASE("Basic") {
+    CHECK((encryptCaesar("Way to Go!",5)) == "Bfd yt Lt!");
+    CHECK((encryptCaesar("I l!Ke to cOde and s@y lies", 8)) == "Q t!Sm bw kWlm ivl a@g tqma");
+    CHECK((encryptCaesar("I don't Know lol", 21)) == "D yji'o Fijr gjg");
+  }
+}
