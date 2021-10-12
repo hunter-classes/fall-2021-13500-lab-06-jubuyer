30a30d3bd6975585682f679a8e6b4733549b0e65
jubuyer <jahmed10@stuy.edu>
2021-10-12 15:13:43 -0400

    Tests for decryptCaesar() added

--- tests.cpp
+++ tests.cpp
@@ -20,10 +20,14 @@ TEST_CASE("Caesar Cipher Theory Tests") {
 
 TEST_CASE("Decrypt Caesar Cipher Theory Tests") {
   SUBCASE("Basic") {
-
+    CHECK((decryptCaesar("Bfd yt Lt!",5)) == "Way to Go!");
+    CHECK((decryptCaesar("Q t!Sm bw kWlm ivl a@g tqma", 8)) == "I l!Ke to cOde and s@y lies");
+    CHECK((decryptCaesar("D yji'o Fijr gjg", 21)) == "I don't Know lol");
   }
   SUBCASE("Extreme") {
-
+    CHECK((decryptCaesar(" ", 2)) == " ");
+    CHECK((decryptCaesar("@*&#!@", 2)) == "@*&#!@");
+    CHECK((decryptCaesar("Is this a dream?", 26)) == "Is this a dream?");
   }
 }
 
