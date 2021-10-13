54b36a50f29fdea8ea26234647dcdf8bac6662f5
jubuyer <jahmed10@stuy.edu>
2021-10-13 00:23:52 -0400

    Added introductory text and a test

--- main.cpp
+++ main.cpp
@@ -5,6 +5,11 @@
 
 int main()
 {
-  
+  std::cout << "This is the main program.\n";
+  std::cout << "To see tests please use the Makefile to run tests.cpp.\n";
+  std::cout << "Examples of tests include (encryptCaesar(\"Way to Go!\",5) == \"Bfd yt Lt!\") ";
+  (encryptCaesar("Way to Go!",5) == "Bfd yt Lt!") ? std::cout << "-> Success!\n"  : std::cout << "-> Failed\n";
+  std::cout << "Input 'make help' to see a list of targets.\n";
+
   return 0;
 }
